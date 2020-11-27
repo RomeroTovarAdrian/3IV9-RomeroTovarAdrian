9e38e13b34e0710f4034fea68ac3f228e0bbfd23
RomeroTovarAdrian <aromerot1900@alumno.ipn.mx>
2020-11-27 14:56:34 -0600

    Examen2POO

--- Examen2POO/Ahorcado.java
+++ Examen2POO/Ahorcado.java
@@ -3,49 +3,53 @@ public class Ahorcado{
     public static void main (String[] args){
         Scanner entrada = new Scanner(System.in);
         String palabra, letra;
-        int vidas, negativo, contador; 
-        System.out.println("Ingrese la palabra");
-        palabra = entrada.nextLine();
-        Integer l=palabra.length();
-        System.out.println(l);
-        String letras[] = new String[l];
-        int mostrador[] = new int[l];
-        for(int i = 0; i<letras.length; i++){
-            letras[i]=palabra.substring(i,i+1);
-        }
-        for(int i = 0; i<letras.length; i++){
-            System.out.println(letras[i]);
-        }
-        vidas=7;
-        contador=l;
+        int vidas, negativo, contador, salida; 
         do{
-            System.out.println("Ingrese la letra que crea que esta en la palabra");
-            letra = entrada.nextLine();
-            negativo=0;
+            System.out.println("Ingrese la palabra");
+            palabra = System.console().readLine();
+            Integer l=palabra.length();
+            String letras[] = new String[l];
+            int mostrador[] = new int[l];
             for(int i = 0; i<letras.length; i++){
-                System.out.println(letras[i]);
-                System.out.println(letra);
-                if(letra.equals(letras[i])){
-                    mostrador[i]=1;
-                    contador-=1;
-                    System.out.println(contador);
-                }else{
-                    negativo+=1;
+                letras[i]=palabra.substring(i,i+1);
+            }
+            vidas=7;
+            contador=l;
+            do{
+                System.out.println("Ingrese la letra que crea que esta en la palabra");
+                letra = System.console().readLine();
+                negativo=0;
+                for(int i = 0; i<letras.length; i++){
+                    if(letra.equals(letras[i])){
+                        mostrador[i]=1;
+                        contador-=1;
+                    }else{
+                        negativo+=1;
+                    }
                 }
-            }
-            System.out.println();
-            for(int i = 0; i<letras.length; i++){
-                if(mostrador[i]==1){
-                    System.out.print(letras[i]+" ");
-                }else{
-                    System.out.print("_ ");
+                System.out.println();
+                for(int i = 0; i<letras.length; i++){
+                    if(mostrador[i]==1){
+                        System.out.print(letras[i]+" ");
+                    }else{
+                        System.out.print("_ ");
+                    }
                 }
+                System.out.println();
+                if(negativo==l){
+                    vidas-=1;
+                }
+                System.out.println("vidas restantes "+vidas);
+            }while(vidas!=0 && contador!=0);
+            if(vidas==0){
+                System.out.println("Perdiste");
+            }else if(contador==0){
+                System.out.println("Ganaste");
             }
-            System.out.println();
-            if(negativo==l){
-                vidas-=1;
-            }
-            System.out.println("vidas restantes "+vidas);
-        }while(vidas!=0 && contador!=0);
+            System.out.println("Que quiere hacer?");
+            System.out.println("1. para jugar de nuevo");
+            System.out.println("0. Para cerrar el programa");
+            salida = entrada.nextInt();
+        }while(salida!=0);
     }
 }
