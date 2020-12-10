2baddfcd0861b6866e00ad6ec8855fd846c8b46d
RomeroTovarAdrian <aromerot1900@alumno.ipn.mx>
2020-12-10 01:02:52 -0600

    Tarea de labo

---
+++ EventosMouse/src/Eventos/mouse/EventosMouse.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Eventos.mouse;
+
+/**
+ *
+ * @author Adrian
+ */
+public class EventosMouse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Window ventana= new Window();
+        ventana.setVisible(true);
+    }
+    
+}
